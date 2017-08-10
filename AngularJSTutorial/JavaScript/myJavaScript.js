c118e502e6999290aaf5ee1c2c2aa258e2358115
Parankusham <sridhar.parankusham@accenture.com>
2017-08-10 23:14:08 +0530

    Today's changes

---
+++ AngularJSTutorial/JavaScript/myJavaScript.js
@@ -0,0 +1,18 @@
+function square(number) {
+  console.log(number * number);
+  console.log(fac(number));
+  console.log(map(fac(),[5,6,7]));
+};
+
+function fac(n) 
+{ 
+  return n < 2 ? 1 : n * fac(n - 1);
+};
+
+function map(fn, ar) {
+  var result = [], // Create a new Array
+      i;
+  for (i = 0; i != a.length; i++)
+    result[i] = fn(ar[i]);
+  return result;
+};
